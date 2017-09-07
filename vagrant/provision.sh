62a8ab738b5219e7f02117890f01cfa85476dbce
Michal Welenc <welencm@gmail.com>
2017-09-07 19:57:45 +0200

    SSH key genration and password change moved from provision.sh to ansible role common; setting correct timezone

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -15,14 +15,3 @@ fi
 echo "[INFO] Authorize ssh keys"
 mkdir -p ${HOME}/.ssh
 cat /vagrant/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
-
-echo "[INFO] Generate SSH key"
-if [ ! -f ${HOME}/.ssh/id_rsa ]; then
-    sudo -u ${USER} ssh-keygen -t rsa -b 2048 -f ${HOME}/.ssh/id_rsa -q -N ""
-    cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
-fi
-
-if [[ "${USER}" == "ubuntu" ]]; then
-  echo "[INFO] Set password for user ${USER}"
-  echo 'ubuntu:$6$IR67Da8C$IE.9h/ECXI4Z9S1h6kgLuRXdrr7Qv4F6UeKzA.tXY.x5Lx7SaTYfD7mTW6dMS1nxF2R62fMVIqNkZVo.Kn8vW0' | sudo chpasswd -e
-fi
